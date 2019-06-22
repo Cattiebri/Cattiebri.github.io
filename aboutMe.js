0ed76149f7001b1b052fee95f8f55ca70a02611d
Cattiebri <52102131+Cattiebri@users.noreply.github.com>
2019-06-23 00:24:03 +0300

    uzlabojumi

---
+++ aboutMe.js
@@ -0,0 +1,12 @@
+function on(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function off() {
+  $(".overlay").hide();
+}
+
+function showtick() {
+	$("#tick").show();
+	$("#licence").hide();
+}
