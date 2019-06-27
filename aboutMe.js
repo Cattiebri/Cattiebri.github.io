9aea4a683eeda8ddee17ae4b1b966554d466b924
Cattiebri <52102131+Cattiebri@users.noreply.github.com>
2019-06-28 01:29:49 +0300

    improvements

--- aboutMe.js
+++ aboutMe.js
@@ -10,3 +10,8 @@ function showtick() {
 	$("#tick").show();
 	$("#licence").hide();
 }
+
+$(document).on('shown.bs.collapse', function(event){
+        //console.log( "in! print e: " +event.type);
+        event.target.scrollIntoView();
+    });
